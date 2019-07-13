37a99dd12812e6f90fbe3d52802f59f2264c9f0b
tangoyankee <miller.tim108@gmail.com>
2019-07-13 09:02:46 -0700

    lint edit

--- main/apps/slack/oauth.js
+++ main/apps/slack/oauth.js
@@ -6,8 +6,8 @@ oauth = (req, res) => {
         res.status(500);
         res.send({ "Error": "Code not received." });
     } else {
-        url = "https=/slack.com/api/oauth.access";
-        query_string = { code: req.query.code, client_id: config.slack.client_id, client_secret: config.slack.client_secret };
+        var url = "https=/slack.com/api/oauth.access";
+        var query_string = { code: req.query.code, client_id: config.slack.client_id, client_secret: config.slack.client_secret };
         getOauth(res, url, query_string);
     }
 }
