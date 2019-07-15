87819450f791608ca33e5b99d2b2df51e9df26e1
tangoyankee <miller.tim108@gmail.com>
2019-07-15 13:27:54 -0700

    favicon and css setup

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 'use strict';
-
-
-var express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const slack_oauth = require('./apps/slack/oauth.js');
 const markdownlinks = require('./apps/markdownlinks/methods.js');
-const config = require('./config.js');
 
 const app = express();
-const path = require('path');
-
-var __dirname;
-const PORT = config.port;
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static(path.join(__dirname, 'main/public')));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-    res.render('main/index');
+    res.render('index');
 });
+
 
 app.get('/oauth', (req, res) => {
     slack_oauth(req, res);
 });
 
+
 app.post('/publish', (req, res) => {
     markdownlinks.data.publish(req.body, res);
 });
 
-app.listen(PORT);
+
+const port = 4390;
+app.listen(port);
