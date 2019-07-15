88310f85610278eb7c60ec35ac9fd31092c2a611
tangoyankee <miller.tim108@gmail.com>
2019-07-15 16:02:11 -0700

    responsive text header

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,19 @@ function footerAlign(){
     $('footer').css('height', footerHeight);
 }
 
+function headerAlign(){
+    $('header').css('height', 'auto');
+    var headerHeight = $('header').outerHeight();
+    $('body').css('padding-top', headerHeight);
+    $('header').css('height', footerHeight);
+}
+
 $(document).ready(()=>{
     footerAlign();
+    headerAlign();
 })
 
 $(window).resize(()=>{
     footerAlign();
+    headerAlign();
 })
