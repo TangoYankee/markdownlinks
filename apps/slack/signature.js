c45027073c5cc0e9f7a2b9e408e4984d5e0f4895
tangoyankee <miller.tim108@gmail.com>
2019-07-21 13:06:32 -0700

    format with rfc1738

--- apps/slack/signature.js
+++ apps/slack/signature.js
@@ -28,37 +28,17 @@ isValidHash = (timestamp, request) => {
     let version = 'v0';
     let request_body = qs.stringify(request.body,{format:'RFC1738'});
     base_string = getBaseString(version, timestamp, request_body);
-    console.log(`base string: ${base_string}`);
     let signing_secret = process.env.SLACK_SIGNING_SECRET;
     hex_digest = getHexDigest(signing_secret, base_string);
-    console.log(`hex digest: ${hex_digest}`);
     app_signature = getSignature(version, hex_digest);
-    console.log(`app sig: ${app_signature}`);
     slack_signature = request.headers['x-slack-signature'];
-    console.log(`slack sig: ${slack_signature}`);
     return (app_signature === slack_signature);
 }
 
 
 getBaseString = (version, timestamp, request_body) => {
     /*format raw string to use in calculation hash*/
-    return (`${version}:${timestamp}:${serializeBody(request_body)}`);
-}
-
-serializeBody = (request_body) => {
-    /*http body should not be deserialized*/
-    let token = request_body.token;
-    let team_id = request_body.team_id;
-    let team_domain = request_body.team_domain;
-    let channel_id = request_body.channel_id;
-    let channel_name = request_body.channel_name;
-    let user_id = request_body.user_id;
-    let user_name = request_body.user_name;
-    let command = request_body.command;
-    let text = request_body.text;
-    let response_url = request_body.response_url;
-    let trigger_id = request_body.trigger_id;
-    return (`token=${token}&team_id=${team_id}&team_domain=${team_domain}&channel_id=${channel_id}&channel_name=${channel_name}&user_id=${user_id}&user_name=${user_name}&command=${command}&text=${text}&response_url=${response_url}&trigger_id=${trigger_id}`);
+    return (`${version}:${timestamp}:${request_body}`);
 }
 
 
