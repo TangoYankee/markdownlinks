9b29b5f1309b4b7584ebfdcad63bd078c393f6c8
tangoyankee <miller.tim108@gmail.com>
2019-08-03 16:17:13 -0700

    connect to database, set logic to save team and access token

--- apps/database/db.js
+++ apps/database/db.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const process = require('process');
 
-// Database testing
-// https://jestjs.io/docs/en/setup-teardown 
-// https://jestjs.io/docs/en/mongodb
+const cluster_uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`;
+const client = new MongoClient(cluster_uri, { useNewUrlParser: true });
 
-var OAuthSchema = new mongoose.Schema({
-    // team_id (string) oauth tokens are stored based on teams
-    // access_token_cipher (string) encrypted token, appended with iv
-})
+
+saveTeam = (team_id, access_token_cipher) => {
+    var team_collection = client.db("markdownlinksdb").collection("teams");
+    var team_record;
+    client.connect(err => {
+        if (err) return console.log(err);
+        console.log("connected successfully")
+        team_record = team_collection.findOne({team_id: team_id});
+        if (team_record) {
+            team_collection.findOneAndUpdate({team_id: team_id},
+                { $set: {access_token_cipher: access_token_cipher}});
+        } else if (!team_record) {
+            team_collection.insertOne({team_id: team_id, access_token_cipher: access_token_cipher})
+        }
+        client.close();
+    })
+}
+
+module.exports = saveTeam;
