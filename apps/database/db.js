cd837e8eeec180a91972025380d125236f25dae4
tangoyankee <miller.tim108@gmail.com>
2019-08-03 19:07:09 -0700

    restructure function for tests. add async

--- apps/database/db.js
+++ apps/database/db.js
@@ -2,24 +2,44 @@ const MongoClient = require('mongodb').MongoClient;
 const process = require('process');
 
 const cluster_uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`;
-const client = new MongoClient(cluster_uri, { useNewUrlParser: true });
+const client = new MongoClient(cluster_uri, {
+    useNewUrlParser: true
+});
 
 
 saveTeam = (team_id, access_token_cipher) => {
-    var team_collection = client.db("markdownlinksdb").collection("teams");
-    var team_record;
+    var db = client.db("markdownlinksdb");
+    var teams = db.collection("teams");
     client.connect(err => {
         if (err) return console.log(err);
         console.log("connected successfully")
-        team_record = team_collection.findOne({team_id: team_id});
-        if (team_record) {
-            team_collection.findOneAndUpdate({team_id: team_id},
-                { $set: {access_token_cipher: access_token_cipher}});
-        } else if (!team_record) {
-            team_collection.insertOne({team_id: team_id, access_token_cipher: access_token_cipher})
-        }
+        checkTeam(team_id, access_token_cipher, db);
         client.close();
-    })
+    });
 }
 
-module.exports = saveTeam;
+checkTeam = async (team_id, access_token_cipher, teams) => {
+    var team_record;
+    console.log(`Record before looking: ${team_record}`)
+    team_record = await teams.findOne({
+        team_id: team_id
+    });
+    console.log(`Record after looking: ${team_record}`)
+    if (team_record) {
+        console.log("found record")
+        await teams.findOneAndUpdate({
+            team_id: team_id
+        },
+            {
+                $set:
+                    { access_token_cipher: access_token_cipher }
+            });
+    } else if (!team_record) {
+        console.log("did not find record")
+        await teams.insertOne({
+            team_id: team_id, access_token_cipher: access_token_cipher
+        });
+    }
+}
+
+module.exports = { saveTeam, checkTeam };
--- apps/database/db.test.js
+++ apps/database/db.test.js
@@ -1,28 +1,32 @@
-const {MongoClient} = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const { saveTeam, checkTeam } = require('./db.js');
+
 
 describe('insert', () => {
-  let connection;
-  let db;
+    let connection;
+    let db;
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
+    beforeAll(async () => {
+        connection = await MongoClient.connect(global.__MONGO_URI__, {
+            useNewUrlParser: true,
+        });
+        db = await connection.db(global.__MONGO_DB_NAME__);
     });
-    db = await connection.db(global.__MONGO_DB_NAME__);
-  });
 
-  afterAll(async () => {
-    await connection.close();
-    await db.close();
-  });
+    afterAll(async () => {
+        await connection.close();
+        await db.close();
+    });
 
-  it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
+    it('should insert a doc into collection', async () => {
+        var teams = db.collection("teams");
+        let team_one_id = '1234567890';
+        let team_one_token = 'qwertyasdfg';
+        await checkTeam(team_one_id, team_one_token, teams);
 
-    const mockUser = {_id: 'some-user-id', name: 'John'};
-    await users.insertOne(mockUser);
+        const inserted_team = await teams.findOne({ team_id: team_one_id });
+        console.log(`test result team: ${inserted_team}`);
+        expect(team_one_token).toEqual(inserted_team.access_token_cipher);
+    });
 
-    const insertedUser = await users.findOne({_id: 'some-user-id'});
-    expect(insertedUser).toEqual(mockUser);
-  });
 });
