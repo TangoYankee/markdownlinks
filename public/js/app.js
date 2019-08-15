7af97e3f543f2787dcb0e698bc4fc19cf8a6ad3f
tangoyankee <miller.tim108@gmail.com>
2019-08-15 08:48:09 -0700

    spell accommodate correctly

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 function footerAlign(){
-  /* create bottom padding on the body to accomadate footer */
+  /* create bottom padding on the body to accommodate footer */
   $('footer').css('height', 'auto');
   var footerHeight = $('footer').outerHeight();
   $('body').css('padding-bottom', footerHeight);
@@ -7,7 +7,7 @@ function footerAlign(){
 }
 
 function headerAlign(){
-  /* create top padding on the body to accomadate header */
+  /* create top padding on the body to accommodate header */
   $('header').css('height', 'auto');
   var headerHeight = $('header').outerHeight();
   $('body').css('padding-top', headerHeight);
