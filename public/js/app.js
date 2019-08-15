f76f07bddb1f0def9e9081d54d1a868311325732
tangoyankee <miller.tim108@gmail.com>
2019-08-15 08:47:16 -0700

    separate header footer calls to perform in parallel

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,31 @@
 function footerAlign(){
-    $('footer').css('height', 'auto');
-    var footerHeight = $('footer').outerHeight();
-    $('body').css('padding-bottom', footerHeight);
-    $('footer').css('height', footerHeight);
+  /* create bottom padding on the body to accomadate footer */
+  $('footer').css('height', 'auto');
+  var footerHeight = $('footer').outerHeight();
+  $('body').css('padding-bottom', footerHeight);
+  $('footer').css('height', footerHeight);
 }
-
 
 function headerAlign(){
-    $('header').css('height', 'auto');
-    var headerHeight = $('header').outerHeight();
-    $('body').css('padding-top', headerHeight);
-    $('header').css('height', headerHeight);
+  /* create top padding on the body to accomadate header */
+  $('header').css('height', 'auto');
+  var headerHeight = $('header').outerHeight();
+  $('body').css('padding-top', headerHeight);
+  $('header').css('height', headerHeight);
 }
 
-
-$(document).ready(()=>{
-    footerAlign();
-    headerAlign();
+$().ready(()=>{
+  footerAlign();
 })
 
+$().ready(()=>{
+  headerAlign();
+})
 
 $(window).resize(()=>{
-    footerAlign();
-    headerAlign();
+  footerAlign();
+})
+
+$(window).resize(()=>{
+  headerAlign();
 })
