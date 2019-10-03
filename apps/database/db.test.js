3fd9726135ddb05902506bd9a1a146e6fc0250e0
tangoyankee <miller.tim108@gmail.com>
2019-10-03 16:58:17 -0700

    lint, refactor db.test.js

--- apps/database/db.test.js
+++ apps/database/db.test.js
@@ -17,22 +17,22 @@ describe('insert', () => {
     await db.close()
   })
 
-  const team_id = '1234567890'
-  const team_token_one = 'qwertyasdfg'
-  const team_token_two = 'plmoknijb'
+  const teamId = '1234567890'
+  const teamTokenOne = 'qwertyasdfg'
+  const teamTokentwo = 'plmoknijb'
   it('should insert a team into the collection', async () => {
     const teams = db.collection('teams')
-    await checkTeam(team_id, team_token_one, teams)
-    const inserted_team = await teams.findOne({ team_id: team_id })
-    expect(team_token_one).toEqual(inserted_team.access_token_cipher)
+    await checkTeam(teamId, teamTokenOne, teams)
+    const insertedTeam = await teams.findOne({ teamId: teamId })
+    expect(teamTokenOne).toEqual(insertedTeam.accessTokenCipher)
   })
 
   it('should update a team with a new token', async () => {
     const teams = db.collection('teams')
-    const inserted_team = await teams.findOne({ team_id: team_id })
-    expect(team_token_one).toEqual(inserted_team.access_token_cipher)
-    await checkTeam(team_id, team_token_two, teams)
-    const updated_team = await teams.findOne({ team_id: team_id })
-    expect(team_token_two).toEqual(updated_team.access_token_cipher)
+    const insertedTeam = await teams.findOne({ teamId: teamId })
+    expect(teamTokenOne).toEqual(insertedTeam.accessTokenCipher)
+    await checkTeam(teamId, teamTokentwo, teams)
+    const updatedTeam = await teams.findOne({ teamId: teamId })
+    expect(teamTokentwo).toEqual(updatedTeam.accessTokenCipher)
   })
 })
