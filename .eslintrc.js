0562bbe2946651580709f1c13ca28692f442fbbc
tangoyankee <miller.tim108@gmail.com>
2019-10-03 16:06:54 -0700

    install support packages for standard style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es6": true,
         "jest": true,
         "node":true
     },
-    "extends": "eslint:recommended",
+    "extends": "standard",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018
-    },
-    "rules": {
-      "no-console": "off"
     }
 };
