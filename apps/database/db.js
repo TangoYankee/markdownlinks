ed534be12a677fe2dfc59e533570a52d767ee814
tangoyankee <miller.tim108@gmail.com>
2019-10-03 16:51:45 -0700

    lint db.js

--- apps/database/db.js
+++ apps/database/db.js
@@ -1,39 +1,38 @@
-const MongoClient = require("mongodb").MongoClient;
-const process = require("process");
+const MongoClient = require('mongodb').MongoClient
+const process = require('process')
 
-var cluster_uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`;
+var clusterUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`
 
-var saveTeam = (team_id, access_token_cipher) => {
-  let client = MongoClient(cluster_uri, { useNewUrlParser: true });
-  client.connect(async(err) => {
-    if (err) return console.log(err);
-    let teams = client.db("markdownlinksdb").collection("teams");
-    console.log("connected successfully");
-    await checkTeam(team_id, access_token_cipher, teams);
-    client.close();
-  });
+var saveTeam = (teamId, accessTokenCipher) => {
+  const client = MongoClient(clusterUri, { useNewUrlParser: true })
+  client.connect(async (err) => {
+    if (err) return console.log(err)
+    const teams = client.db('markdownlinksdb').collection('teams')
+    console.log('connected successfully')
+    await checkTeam(teamId, accessTokenCipher, teams)
+    client.close()
+  })
 }
 
-var checkTeam = async (team_id, access_token_cipher, teams) => {
-  let team_record;
-  team_record = await teams.findOne({
-    team_id: team_id
-  });
-  if (team_record) {
-    console.log("found record");
+var checkTeam = async (teamId, accessTokenCipher, teams) => {
+  const teamRecord = await teams.findOne({
+    teamId: teamId
+  })
+  if (teamRecord) {
+    console.log('found record')
     await teams.findOneAndUpdate({
-      team_id: team_id
+      teamId: teamId
     },
-      {
-        $set:
-          { access_token_cipher: access_token_cipher }
-      });
-  } else if (!team_record) {
-    console.log("did not find record")
+    {
+      $set:
+          { accessTokenCipher: accessTokenCipher }
+    })
+  } else if (!teamRecord) {
+    console.log('did not find record')
     await teams.insertOne({
-      team_id: team_id, access_token_cipher: access_token_cipher
-    });
+      teamId: teamId, accessTokenCipher: accessTokenCipher
+    })
   }
 }
 
-module.exports = { saveTeam, checkTeam };
+module.exports = { saveTeam, checkTeam }
