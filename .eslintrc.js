31888211aecbd86d21468089d626f5de72152500
tangoyankee <miller.tim108@gmail.com>
2019-10-03 16:44:30 -0700

    lint js files. turn off definition requirement for js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
     "extends": "standard",
     "parserOptions": {
         "ecmaVersion": 2019
-    }
+    },
+    "overrides": [
+        {
+            "files": [ "public/js/*.js" ],
+            "rules": {
+                "no-undef": "off"
+            }
+        }
+    ]
 };
