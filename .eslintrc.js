f8fa46f4eaa082b19eb8bc6c5079a302c61a97ed
tangoyankee <miller.tim108@gmail.com>
2019-10-04 08:04:22 -0700

    eslint quote exceptions for messages. change public no-undef from off to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,19 @@ module.exports = {
         "ecmaVersion": 2019
     },
     "overrides": [
-        {
+        {   /* datalayer and jquery's '$' are not recongized by eslint */
             "files": [ "public/js/*.js" ],
             "rules": {
-                "no-undef": "off"
+                "no-undef": "warn"
             }
+        },
+        {   /* slack's block kit builder is specific on quotes syntax */
+            "files": [ "apps/markdownlinks/messages.js" ],
+            "rules": {
+                "quote-props": "warn",
+                "quotes": "warn"
+            }
+
         }
     ]
 };
