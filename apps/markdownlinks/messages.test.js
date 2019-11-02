af4eef18f8c4808cce826c5df6b8065d776a5ac9
tangoyankee <miller.tim108@gmail.com>
2019-11-02 12:14:26 -0700

    abstract the warnings into emojis and text. refactor functions to build slack message blocks

--- apps/markdownlinks/messages.test.js
+++ apps/markdownlinks/messages.test.js
@@ -1,94 +1,91 @@
 const messages = require('./messages.js')
 
 test('format message based on object data', () => {
-    var messageData = {
-        "message": "[Social Engineering Site](http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/), [Malware Site](testsafebrowsing.appspot.com/s/malware.html), and [Nasa](https://nasa.gov)",
-        "lookupError": "false",
-        "sharedBy": "TangoYankee",
-        "safeBrowseSuccess": true,
-        "allSharedAsHttpSecure": false,
-        "threatTypes": [
-          "SOCIAL_ENGINEERING",
-          "MALWARE"
-        ],
-        "links": [
+  var messageData = {
+    "message": "[Social Engineering Site](http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/), [Malware Site](testsafebrowsing.appspot.com/s/malware.html), and [Nasa](https://nasa.gov)",
+    "sharedBy": "TangoYankee",
+    "safeBrowseSuccess": true,
+    "allSharedAsHttpSecure": false,
+    "threatTypes": [
+      "SOCIAL_ENGINEERING",
+      "MALWARE"
+    ],
+    "links": [
+      {
+        "cacheKeyFromUrl": "testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/",
+        "cacheDuration": "300s",
+        "markdownLink": "[Social Engineering Site](http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/)",
+        "messageLink": "<http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/|Social Engineering Site>",
+        "sharedAsHttpSecure": false,
+        "threatMatch": "SOCIAL_ENGINEERING"
+      },
+      {
+        "cacheKeyFromUrl": "testsafebrowsing.appspot.com/s/malware.html",
+        "cacheDuration": "300s",
+        "markdownLink": "[Malware Site](testsafebrowsing.appspot.com/s/malware.html)",
+        "messageLink": "<https://testsafebrowsing.appspot.com/s/malware.html|Malware Site>",
+        "sharedAsHttpSecure": false,
+        "threatMatch": "MALWARE"
+      },
+      {
+        "cacheKeyFromUrl": "https://nasa.gov",
+        "cacheDuration": "",
+        "markdownLink": "[Nasa](https://nasa.gov)",
+        "messageLink": "<https://nasa.gov|Nasa>",
+        "sharedAsHttpSecure": true,
+        "threatMatch": ""
+      }
+    ]
+  }
+
+  var messageFormat = {
+    "response_type": "in_channel",
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "<http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/|Social Engineering Site>:biohazard_sign::eyes:, <https://testsafebrowsing.appspot.com/s/malware.html|Malware Site>:beetle::eyes:, and <https://nasa.gov|Nasa>"
+        }
+      },
+      {
+        "type": "context",
+        "elements": [
           {
-            "cacheKeyFromUrl": "testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/",
-            "cacheDuration": "300s",
-            "markdownLink": "[Social Engineering Site](http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/)",
-            "messageLink": "<http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/|Social Engineering Site>",
-            "sharedAsHttpSecure": false,
-            "threatMatch": "SOCIAL_ENGINEERING"
+            "type": "mrkdwn",
+            "text": "-shared by @TangoYankee"
           },
           {
-            "cacheKeyFromUrl": "testsafebrowsing.appspot.com/s/malware.html",
-            "cacheDuration": "300s",
-            "markdownLink": "[Malware Site](testsafebrowsing.appspot.com/s/malware.html)",
-            "messageLink": "<https://testsafebrowsing.appspot.com/s/malware.html|Malware Site>",
-            "sharedAsHttpSecure": false,
-            "threatMatch": "MALWARE"
-          },
+            "type": "mrkdwn",
+            "text": ":eyes: shared without <https://www.snopes.com/fact-check/http-vs-https/|https>"
+          }
+        ]
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "context",
+        "elements": [
           {
-            "cacheKeyFromUrl": "https://nasa.gov",
-            "cacheDuration": "",
-            "markdownLink": "[Nasa](https://nasa.gov)",
-            "messageLink": "<https://nasa.gov|Nasa>",
-            "sharedAsHttpSecure": true,
-            "threatMatch": ""
+            "type": "mrkdwn",
+            "text": ":warning: Suspected threats found by <https://developers.google.com/safe-browsing/v4/advisory|Google Safe Browse>:"
+          }
+        ]
+      },
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "mrkdwn",
+            "text": ":biohazard_sign: <https://googleonlinesecurity.blogspot.com/2015/11/safe-browsing-protection-from-even-more.html|social engineering>"
+          }, {
+            "type": "mrkdwn",
+            "text": ":beetle: <https://www.stopbadware.org/|malware> "
           }
         ]
       }
-    
-      var messageFormat = {
-        "response_type": "in_channel",
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "<http://testsafebrowsing.appspot.com/apiv4/OSX/SOCIAL_ENGINEERING/URL/|Social Engineering Site>:biohazard_sign::eyes:, <https://testsafebrowsing.appspot.com/s/malware.html|Malware Site>:beetle::eyes:, and <https://nasa.gov|Nasa>"
-                }
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": "-shared by @TangoYankee"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": ":eyes: shared without <https://www.snopes.com/fact-check/http-vs-https/|https>"
-                    }
-                ]
-            },
-            {
-                "type": "divider"
-            },
-             {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": ":warning: Suspected threats found by <https://developers.google.com/safe-browsing/v4/advisory|Google Safe Browse>:"
-                    }
-                ]
-            },
-            {
-                "type": "context",
-                "elements": [
-
-                    {
-                        "type": "mrkdwn",
-                        "text": ":biohazard_sign: <https://googleonlinesecurity.blogspot.com/2015/11/safe-browsing-protection-from-even-more.html|social engineering>"
-                    },                    {
-                        "type": "mrkdwn",
-                        "text": ":beetle: <https://www.stopbadware.org/|malware> "
-                    }
-                ]
-            }
-        ]
-      }
-
-    expect(messages.data.devMarkdownMessage(messageData)).toEqual(messageFormat)
+    ]
+  }
+  expect(messages.data.devMarkdownMessage(messageData)).toEqual(messageFormat)
 })
