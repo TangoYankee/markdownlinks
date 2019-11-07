6c41885bf5eacc163ba502a1e855f7b83b72927a
tangoyankee <miller.tim108@gmail.com>
2019-11-07 09:10:32 -0800

    remove old custom-request function

--- async-tutorial/custom-request.js
+++ async-tutorial/custom-request.js
@@ -1,16 +1,6 @@
 const http = require('http')
 const request = require('request')
 
-// const customRequest = (url) => {
-//     return new Promise(resolve => {
-//         http.get({path: url}, response => {
-//             let data = ''
-//             response.on('data', _data => (data+=_data))
-//             response.on('end', () => resolve(data))
-//         })
-//     })
-// }
-
 const customRequest = (url) => {
     return new Promise(resolve => {
         request.get({
