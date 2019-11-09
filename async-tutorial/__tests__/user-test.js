495a5d791529968efb111ebd9828cf59f00d8a0b
tangoyankee <miller.tim108@gmail.com>
2019-11-08 20:28:32 -0800

    remove original jest async tutorial files

--- async-tutorial/__tests__/user-test.js
+++
@@ -1,9 +0,0 @@
-jest.mock('../custom-request')
-
-const { getUserName } = require('../user')
-
-it('works with promises', async () => {
-    expect.assertions(1)
-    const data = await getUserName(4)
-    return expect(data).toEqual('mark')
-})
